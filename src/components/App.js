492972801be0f807748af46a668dac9372a1cc45
Jalen <mnenejalen72@gmail.com>
2024-04-25 17:31:37 +0300

    React Components Basics Lab

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,24 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h2>About Us</h2>
+      <p>
+        Welcome to the website! This is a React Components Basics Lab test
+        website.
+      </p>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
